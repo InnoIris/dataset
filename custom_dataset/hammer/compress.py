df71b2cf73b1f1026a81e951e5a0fd3c0c347436
mod-1 <singhdivyansh98@gmail.com>
2020-05-01 19:58:33 +0530

    Scales the annotations based on image resizing

--- custom_dataset/hammer/compress.py
+++ custom_dataset/hammer/compress.py
@@ -2,6 +2,8 @@ import os
 import sys
 import cv2
 from PIL import Image
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
 
 def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
     # initialize the dimensions of the image to be resized and
@@ -32,13 +34,52 @@ def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
     resized = cv2.resize(image, dim, interpolation = inter)
 
     # return the resized image
-    return resized
+    return resized,r
+
+def annotations(file, ratio):
+    filename_xml = file[:-4] + '.xml'
+    try:
+        tree = ET.parse(os.path.join(os.getcwd(), filename_xml))
+        root = tree.getroot()
+        #considers oen and only one obj exists in the image 
+        x2 = root.find('object').find('bndbox').find('xmin').text 
+        y2 = root.find('object').find('bndbox').find('ymin').text 
+        x1 = root.find('object').find('bndbox').find('xmax').text 
+        y1 = root.find('object').find('bndbox').find('ymax').text
+        x2 = float(x2) * ratio
+        x1 = float(x1) * ratio
+        y1 = float(y1) * ratio
+        y2 = float(y2) * ratio
+        root.find('object').find('bndbox').find('xmin').text = str(int(x2))
+        root.find('object').find('bndbox').find('ymin').text = str(int(y2))
+        root.find('object').find('bndbox').find('xmax').text = str(int(x1))
+        root.find('object').find('bndbox').find('ymax').text = str(int(y1))
+
+        xmlstr = minidom.parseString(ET.tostring(root)).toprettyxml(indent="   ")
+        with open(os.path.join(os.getcwd(), filename_xml), "w") as f:
+            f.write(xmlstr)
+        return (int(x2), int(y2), int(x1), int(y1))
+    except xml.etree.ElementTree.ParseError as e:
+        print (file, str(e))
+        return
+
 
 def compressMe(file, verbose=False):
     filepath = os.path.join(os.getcwd(), file)
     img = cv2.imread(filepath)
-    image = image_resize(img, width = 440)
-    print(image.shape)
+    image, ratio = image_resize(img, width = 440)
+    x1,y1,x2,y2 = annotations(file,ratio)
+    cv2.imwrite(filepath, image)
+    # visualize(image, x1,y1,x2,y2)
+    # print(image.shape)
+
+
+def visualize(image, x1, y1,x2,y2):
+    demo = cv2.rectangle(image, (x1,y1), (x2,y2), (255, 0, 0) , 2)
+    cv2.imshow("test", demo)
+    key = cv2.waitKey(500)#pauses for 0.5 seconds before fetching next image
+    if key == 27:#if ESC is pressed, exit loop
+        cv2.destroyAllWindows()  
 	
 
 def main():
@@ -49,6 +90,7 @@ def main():
         if os.path.splitext(file)[1].lower() in ('.jpg', '.jpeg'):
             num += 1
             compressMe(file)
+    print(num)
     print ("Done")
 	
 
