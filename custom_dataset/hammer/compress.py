64a0aead525029a188081b536c5c10ade7c86d44
mod-1 <singhdivyansh98@gmail.com>
2020-05-01 18:08:47 +0530

    Adds compression script for images

---
+++ custom_dataset/hammer/compress.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import cv2
+from PIL import Image
+
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the       
+        # # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
+
+def compressMe(file, verbose=False):
+    filepath = os.path.join(os.getcwd(), file)
+    img = cv2.imread(filepath)
+    image = image_resize(img, width = 440)
+    print(image.shape)
+	
+
+def main():
+    pwd = os.getcwd()
+    tot = 0
+    num = 0
+    for file in os.listdir(pwd):
+        if os.path.splitext(file)[1].lower() in ('.jpg', '.jpeg'):
+            num += 1
+            compressMe(file)
+    print ("Done")
+	
+
+if __name__ == "__main__":
+	main()
